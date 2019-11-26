bc430fe56bb3c55deb8ccf3560ce72d679e1714d
Thomas Perrier <Thomas_Perrier@insideranken.org>
2019-11-26 14:51:46 -0600

    Add project files.

---
+++ RecordStoreProject2/Models/PartialClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecordStoreProject2.Models
+{
+    [MetadataType(typeof(GenreMetadata))]
+    public partial class Genre
+    {
+    }
+
+    [MetadataType(typeof(ArtistMetadata))]
+    public partial class Artist
+    {
+    }
+
+    [MetadataType(typeof(AlbumMetadata))]
+    public partial class Album
+    {
+    }
+}
---
+++ RecordStoreProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecordStoreProject2.Startup))]
+namespace RecordStoreProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
